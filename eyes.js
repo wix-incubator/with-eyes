807f1d948c0085ed16f17404ea2a027eea136281
Jonathan Adler <jonathana@wix.com>
2019-09-26 16:35:21 +0300

    Applitools server url support (#5)
    * Applitools server url support
    
    If env variable exists
    
    * Update eyes.js
    
    Change env variable name to tomp's PR (wix-incubator/match-screenshot#50)
    
    * Update eyes.js
    
    fix mistake: use instance instead of eyes

--- eyes.js
+++ eyes.js
@@ -36,6 +36,10 @@ if (EYES_API_KEY) {
   instance.setOs(EYES_PLATFORM || process.platform);
   instance.setApiKey(EYES_API_KEY);
   instance.setBatch(name, EYES_BATCH_UUID || uuid.v4());
+  
+  if (process.env.EYES_API_SERVER_URL) {
+    instance.setServerUrl(process.env.EYES_API_SERVER_URL);
+  }
 
   eyes.checkImage = instance.checkImage.bind(instance);
   eyes.abortIfNotClosed = instance.abortIfNotClosed.bind(instance);
